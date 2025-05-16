221173201ec76e308a532f9d3e678edd73dfa2a6
Liam Calamba <133558982+zephyrowwa@users.noreply.github.com>
2025-05-16 22:20:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     model = load_model()
     input_tensor = preprocess_image(image)
